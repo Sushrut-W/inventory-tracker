96bc7b8ea359b9135a5fc4ebc9c16a5bfffbb302
Sushrut Warekar <sware8@illinois.edu>
2024-08-03 19:20:53 -0500

    finished search, started frontend

--- app/page.js
+++ app/page.js
@@ -3,24 +3,42 @@ import {Image} from "next/image";
 import {useState, useEffect} from 'react'
 import {firestore} from '@/firebase'
 import { Box, Modal, Button, Stack, TextField, Typography } from "@mui/material";
+// import HomeIcon from '@mui/icons-material/Home';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import { query, collection, getDocs, getDoc, doc, setDoc, deleteDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState('');
+  const [searchName, setSearchName] = useState('');
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'));
     const docs = await getDocs(snapshot);
     const inventoryList = [];
-    docs.forEach((doc) =>{
+    docs.forEach((doc) => {
       inventoryList.push({
         name: doc.id,
         ...doc.data(),
       })
     })
     setInventory(inventoryList);
+  }
+
+  const searchFor = async (item) => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const searchList = [];
+    docs.forEach((doc) => {
+      if ((doc.id).includes(item)){
+        searchList.push({
+          name: doc.id,
+          ...doc.data(),
+        })
+      }
+    })
+    setInventory(searchList);
   }
 
   const addItem = async (item) => {
@@ -60,35 +78,24 @@ export default function Home() {
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  /* Use colors:
+   *  Red   - #E61031
+   *  Blue  - #005BAD
+   *  White - #FFFFFF
+   *  Grey  - #F2F2F2
+  */
   return(
-    <Box
-      width='100vw'
-      height='100vh'
-      display='flex'
-      flexDirection={'column'}
-      justifyContent='center'
-      alignItems='center'
-      gap='2'
-    >
+    <Box width='100vw' height='100vh' display='flex' flexDirection={'column'} justifyContent='center' alignItems='center' gap='2' bgcolor='#f2f2f2'>
       <Modal open={open} onClose={handleClose}>
         <Box
-          position='absolute'
-          top='50%'
-          left='50%'
-          width={400}
-          bgcolor='white'
-          border='2px solid black'
-          boxShadow={24}
-          p={4}
-          display={"flex"}
-          flexDirection={"column"}
-          gap={3}
+          position='absolute' top='50%' left='50%' width={400}
+          bgcolor='#f2f2f2' border='2px solid black' boxShadow={24}
+          p={4} display={"flex"} flexDirection={"column"} gap={3}
           sx={{
             transform:'translate(-50%, -50%)',
           }}
         >
-          <Typography variant='h6'>Add Item</Typography>
+          <Typography variant='h6' color='#E61031'>Add Item</Typography>
           <Stack width='100%' direction='row' spacing={2}>
             <TextField 
               variant='outlined'
@@ -108,7 +115,7 @@ export default function Home() {
             />
             <Button
               variant='outlined'
-               onClick={()=>{
+              onClick={()=>{
                 addItem(itemName.toLowerCase())
                 setItemName('')
                 handleClose()
@@ -119,24 +126,40 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      <Button
-        variant='contained'
-        onClick={()=>{
-          handleOpen()
-        }}
-      >
-        Add New Item
-      </Button>
+      
+      <Box width='800px' height='50px' bgcolor='#170B3B' alignItems='center' justifyContent='space-between' display='flex'>
+        <Button variant='contained'
+          onClick={()=>{
+            handleOpen();
+          }}
+          >
+          Add New Item
+        </Button>
+        <Stack direction='row' spacing={1}>
+          <TextField width='200px' size='small' variant='outlined' placeholder='Item name' type='search'
+            onChange={(e)=>{
+              setSearchName((e.target.value).toLowerCase());
+            }}
+          />
+          <Button variant='contained'
+            onClick={()=>{
+              searchFor(searchName.toLowerCase());
+            }}
+          >
+            Search
+          </Button>
+          <Button variant='contained' sx={{m:2}} startIcon={<FormatListBulletedIcon/>}
+            onClick={() => {
+              updateInventory();
+            }}
+          > Show all
+          </Button>
+        </Stack>
+      </Box>
+
       <Box border='1px solid red'>
-        <Box
-        width='800px'
-        height='100px'
-        bgcolor='green'
-        alignItems={'center'}
-        justifyContent={'center'}
-        display={'flex'}
-        >
-          <Typography variant='h2' color='blue'>
+        <Box width='800px' height='100px' bgcolor='#170B3B' alignItems={'center'} justifyContent={'center'} display={'flex'}>
+          <Typography variant='h2' color='#9388A2'>
             Inventory Items
           </Typography>
         </Box>
@@ -151,7 +174,7 @@ export default function Home() {
               display='flex'
               alignItems={'center'}
               justifyContent={'space-between'}
-              bgcolor='orange'
+              bgcolor='#9388A2'
               padding={5}
               >
                 <Typography variant='h3' color='purple' textAlign={'center'}>
@@ -161,16 +184,14 @@ export default function Home() {
                 <Typography variant='h3' color='black' textAlign={'center'}>
                   {quantity}
                 </Typography>
-                  <Button
-                    variant='contained'
+                  <Button variant='contained' color='success'
                     onClick={()=>{
                       addItem(name)
                     }}
                   >
                     Add
                   </Button>
-                  <Button
-                    variant='contained'
+                  <Button variant='contained' color='error'
                     onClick={()=>{
                       removeItem(name)
                     }}
